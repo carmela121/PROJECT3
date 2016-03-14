b757bdf312673efec0ab9e97e651ffe8a708e54e
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-03-14 14:19:12 +0000

    fixed seed file so now working

--- api/models/user.js
+++ api/models/user.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
-  username: { type:String, unique:true, required: true ,
-  email: type: String, unique: true, required: true },
-  passwordHash: { type: String, unique:true, required: true },
+  username: { type:String, unique:true, required: true} ,
+  email: {type: String, unique: true, required: true },
+  passwordHash: {type: String, unique:true, required: true },
   dob:Date,
   prefferredZen: String,
   currentLocation:String,
