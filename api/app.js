66e9f5106d604be5c473a2b93049905b47e5efdb
carmela121 <mcarmencarmona@hotmail.com>
2016-03-14 10:57:17 +0000

    Adding tokens and cors

--- api/app.js
+++ api/app.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var app = express();
-var mongoose = require('mongoose');
-var router = require('./config/routes');
+var express       = require('express');
+var app           = express();
+var morgan        = require('morgan');
+var cors          = require('cors');
+var port          = process.env.PORT || 3000;
+var mongoose      = require('mongoose');
+var bodyParser    = require('body-parser');
+var router        = require('./config/routes');
+var jwt           = require('jsonwebtoken');
+var secret        = require('./config/tokens').secret;
 
 mongoose.connect('mongodb://localhost/project3');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
