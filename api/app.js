2631595d291cfec8cb6286cf4c815f764ffd367d
carmela121 <mcarmencarmona@hotmail.com>
2016-03-14 10:51:38 +0000

    completing app.js

--- api/app.js
+++ api/app.js
@@ -7,3 +7,12 @@ var router = require('./config/routes');
 
 mongoose.connect('mongodb://localhost/project3');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', router);
+
+app.listen(port, function() {
+  console.log("Express is listening on port " + port);
+});
