ddb2dd35b8de232b9c2d5e9ac216ba8d2b614eb7
carmela121 <mcarmencarmona@hotmail.com>
2016-03-14 10:41:52 +0000

    Setting up app.js

---
+++ api/app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+var router = require('./config/routes');
