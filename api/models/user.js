b16c46160421f35166022f803ea41639811137f3
Antony Parker <antonyqparker@gmail.com>
2016-03-14 11:10:12 +0000

    completed but untested.  No database has been created yet

--- api/models/user.js
+++ api/models/user.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
 var userSchema = mongoose.Schema({
-  username: String,
-  email: String,
+  username: { type:String, unique:true, required: true ,
+  email: type: String, unique: true, required: true },
+  passwordHash: { type: String, unique:true, required: true },
   dob:Date,
   prefferredZen: String,
   currentLocation:String,
