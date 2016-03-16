176161218a2e3987b2de0ee546f657d947a59e01
carmela121 <mcarmencarmona@hotmail.com>
2016-03-16 10:41:51 +0000

    Added seeds file

---
+++ db/seeds-users.js
@@ -0,0 +1,79 @@
+var mongoose = require("mongoose");
+var User = require('../models/user');
+
+
+var Spot = require('../models/spot');
+var rp       = require("request-promise");
+// var parser   = require('xml2json');
+
+
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/project3';
+mongoose.connect(mongoURI);
+
+//
+// exports.randeats = function(req, res){
+  // var key = req.query.key;
+  // var location = encodeURIComponent(req.query.location);
+  // var radius = 16000;
+  // var sensor = false;
+  // var types = "restaurant";
+  //
+  // // var https = require('https');
+  // var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDPoIujIF8uLPDuTaJ32f-wW0EDd5hCfFg&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types;
+  // console.log(url);
+//   https.get(url, function(response) {
+//       var body ='';
+//       response.on('data', function(chunk) {
+//         body += chunk;
+//       });
+//
+//       response.on('end', function() {
+//         var places = JSON.parse(body);
+//         var locations = places.results;
+//         var randLoc = locations[Math.floor(Math.random() * locations.length)];
+//
+//         res.json(randLoc);
+//       });
+//     }).on('error', function(e) {
+//       console.log("Got error: " + e.message);
+//     });
+//   };
+
+User.collection.drop();
+
+User.create([{
+  username: "TonyP",
+  email: "tony@tony.com",
+  password: "password",
+  passwordConfirmation: "password",
+  dob:"01-01-01",
+  prefferredZen: "Parks",
+  currentLocation:"Second Home",
+  gender: "male",
+  profilePic:"http://www.lcfc.com/images/common/bg_player_profile_default_big.png"
+},{
+  username: "CamC",
+  email: "carmen@carmen.com",
+  password: "password",
+  passwordConfirmation: "password",
+  dob:"02-02-02",
+  prefferredZen: "Bars",
+  currentLocation:"Second Home",
+  gender: "female",
+  profilePic:"http://www.freelanceme.net/Images/default%20profile%20picture.png"
+}, {
+  username: "JayJ",
+  email: "Jay@json.com",
+  password: "password",
+  passwordConfirmation: "password",
+  dob:"03-03-03",
+  prefferredZen: "Cafes",
+  currentLocation:"Second Home",
+  gender: "male",
+  profilePic:"http://vignette4.wikia.nocookie.net/detectiveconan96/images/7/72/Generic_Male_Profile.jpg/revision/latest?cb=20140709000724"
+}],
+  function(err, users){
+        if(err) return console.error(err);
+        console.log(users);
+        mongoose.connection.close();
+      });
--- db/seeds.js
+++ db/seeds.js
@@ -1,89 +1,65 @@
-var mongoose = require("mongoose");
-var User = require('../models/user');
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var database = require('../config/database');
+mongoose.Promise = Promise;
+mongoose.connect(database.uri);
 
-
+var key = process.env.GMAPS_SERVER_KEY;
+var placesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.5152,-0.0722&radius=15000&key=" + key
+var request = require('request-promise');
 var Spot = require('../models/spot');
-var rp       = require("request-promise");
-// var parser   = require('xml2json');
+Spot.collection.drop();
 
+function getDataFromRes(res, type) {
+  var places = JSON.parse(res);
 
-var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/project3';
-mongoose.connect(mongoURI);
+  var data = places.results.map(function(place) {
+    return {
+      placeId: place.id,
+      name: place.name,
+      rating: place.rating,
+      lat: place.geometry.location.lat,
+      lng: place.geometry.location.lng,
+      zenType: type,
+      vicinity: place.vicinity
+    }
+  });
 
-//
-// exports.randeats = function(req, res){
-  // var key = req.query.key;
-  // var location = encodeURIComponent(req.query.location);
-  // var radius = 16000;
-  // var sensor = false;
-  // var types = "restaurant";
-  //
-  // // var https = require('https');
-  // var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDPoIujIF8uLPDuTaJ32f-wW0EDd5hCfFg&location=" + location + "&radius=" + radius + "&sensor=" + sensor + "&types=" + types;
-  // console.log(url);
-//   https.get(url, function(response) {
-//       var body ='';
-//       response.on('data', function(chunk) {
-//         body += chunk;
-//       });
-//
-//       response.on('end', function() {
-//         var places = JSON.parse(body);
-//         var locations = places.results;
-//         var randLoc = locations[Math.floor(Math.random() * locations.length)];
-//
-//         res.json(randLoc);
-//       });
-//     }).on('error', function(e) {
-//       console.log("Got error: " + e.message);
-//     });
-//   };
+  return data;
+}
 
+request(placesURL + "&type=park")
+  .then(function(res) {
+    return Spot.create(getDataFromRes(res, "park"));
+  })
+  .then(function() {
+    console.log("Parks added...");
+    return request(placesURL + "&type=museum");
+  })
+  .then(function(res) {
+    return Spot.create(getDataFromRes(res, "museum"));
+  })
+  .then(function() {
+    console.log("Museums added...");
+    return request(placesURL + "&type=spa")
+  })
+  .then(function(res) {
+    return Spot.create(getDataFromRes(res, "spa"));
+  })
+  .then(function() {
+    console.log("Spas added...");
+    return request(placesURL + "&type=art_gallery");
+  })
+  .then(function(res) {
+    return Spot.create(getDataFromRes(res, "art gallery"));
+  })
+  .then(function() {
+    console.log("Galleries added...");
+  })
+  .catch(function(err) {
+    console.error("Oops, something went wrong!", err);
+  })
+  .finally(function() {
+    mongoose.connection.close();
+  });
 
-
-
-
-
-
-
-
-
-
-User.collection.drop();
-
-User.create([{
-  username: "TonyP",
-  email: "tony@tony.com",
-  password: "password",
-  passwordConfirmation: "password",
-  dob:"01-01-01",
-  prefferredZen: "Parks",
-  currentLocation:"Second Home",
-  gender: "male",
-  profilePic:"http://www.lcfc.com/images/common/bg_player_profile_default_big.png"
-},{
-  username: "CamC",
-  email: "carmen@carmen.com",
-  password: "password",
-  passwordConfirmation: "password",
-  dob:"02-02-02",
-  prefferredZen: "Bars",
-  currentLocation:"Second Home",
-  gender: "female",
-  profilePic:"http://www.freelanceme.net/Images/default%20profile%20picture.png"
-}, {
-  username: "JayJ",
-  email: "Jay@json.com",
-  password: "password",
-  passwordConfirmation: "password",
-  dob:"03-03-03",
-  prefferredZen: "Cafes",
-  currentLocation:"Second Home",
-  gender: "male",
-  profilePic:"http://vignette4.wikia.nocookie.net/detectiveconan96/images/7/72/Generic_Male_Profile.jpg/revision/latest?cb=20140709000724"
-}],
-  function(err, users){
-        if(err) return console.error(err);
-        console.log(users);
-        mongoose.connection.close();
-      });
